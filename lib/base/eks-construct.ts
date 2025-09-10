f58b3d703fdeb23a593f5cb4f5dc70d380a75135
Thang Nguyen <mrwin95@gmail.com>
2025-09-10 12:27:40 -0600

    add kubectl layer to eks

--- lib/base/eks-construct.ts
+++ lib/base/eks-construct.ts
@@ -1,5 +1,9 @@
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as eks from "aws-cdk-lib/aws-eks";
+import { KubectlV31Layer } from "@aws-cdk/lambda-layer-kubectl-v31";
+import { KubectlV32Layer } from "@aws-cdk/lambda-layer-kubectl-v32";
+import { KubectlV33Layer } from "@aws-cdk/lambda-layer-kubectl-v33";
+
 import { Construct } from "constructs";
 
 export interface EksConstructProps {
@@ -14,13 +18,28 @@ export class EksConstruct extends Construct {
   constructor(scope: Construct, id: string, props: EksConstructProps) {
     super(scope, id);
 
+    const version = props.version ?? eks.KubernetesVersion.V1_31;
+
     this.cluster = new eks.Cluster(this, "EKSCluster", {
       vpc: props.vpc,
       clusterName: props.clusterName,
       version: props.version ?? eks.KubernetesVersion.V1_30,
       defaultCapacity: 2,
       defaultCapacityInstance: new ec2.InstanceType("t3.medium"),
-      //   kubectlLayer: new eks.KubectlLayer(this, "KubectlLayer"),
+      kubectlLayer: this.resolveKubectlLayer(version),
     });
+  }
+
+  private resolveKubectlLayer(version: eks.KubernetesVersion) {
+    switch (version.version) {
+      case "1.31":
+        return new KubectlV31Layer(this, "KubectlV31Layer");
+      case "1.32":
+        return new KubectlV32Layer(this, "KubectlV32Layer");
+      case "1.33":
+        return new KubectlV33Layer(this, "KubectlV33Layer");
+      default:
+        return new KubectlV31Layer(this, "KubectlV31Layer");
+    }
   }
 }
--- lib/environments/dev-eks-stack.ts
+++ lib/environments/dev-eks-stack.ts
@@ -2,7 +2,7 @@ import { Stack, StackProps, Fn } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import { EksConstruct } from "../base/eks-construct";
-import * as cdk from "aws-cdk-lib";
+
 export class DevEksStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
